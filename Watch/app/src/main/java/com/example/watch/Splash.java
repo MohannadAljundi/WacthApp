73cd697221410d93396cf8aed0e655a2e516f7cc
MohannadAljundi <61085040+MohannadAljundi@users.noreply.github.com>
2020-07-20 11:11:13 +0300

    Update

--- Watch/app/src/main/java/com/example/watch/Splash.java
+++ Watch/app/src/main/java/com/example/watch/Splash.java
@@ -32,7 +32,7 @@ public class Splash extends AppCompatActivity {
             @Override
             public void run() {
                 /* Create an Intent that will start the Menu-Activity. */
-                Intent mainIntent = new Intent(Splash.this, BluetoothActivity.class);
+                Intent mainIntent = new Intent(Splash.this, MainActivity.class);
                 Splash.this.startActivity(mainIntent);
                 Splash.this.finish();
             }
